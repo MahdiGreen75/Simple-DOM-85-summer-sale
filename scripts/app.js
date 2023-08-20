3093dfb2234b5164332047876df202aff3a2d49b
MahdiGreen75 <qarimahdi217@gmail.comF>
2023-08-21 01:02:04 +0600

    all features implemented except the modal

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,52 @@
 /****
+ *   Here, 9 cards conveys same kind of handler function and data. since, the data
+ *   between those cards shares the same aspects, we make that common format of 
+ *   data as the following object. as well as the common functionallity inspired
+ *   me to build a common decorator "clickHandlerProducer" function.
+ *   
  *   obj = {
  *      name: 
  *      price:
  *   }
- * 
- * 
- * 
  */
+function setNumToElement(whereToSet, num) {
+    const element = document.getElementById(whereToSet);
+    element.innerText = num;
+}
+function getNumFromElement(id) {
+    const element = document.getElementById(id);
+    const str = element.innerText;
+    const num = +parseFloat(str).toFixed(2);
+    return num;
+}
 function clickHandlerProducer(obj) {
-    function setNumToElement(whereToSet, num, unit = "") {
-        const element = document.getElementById(whereToSet);
-        element.innerText = num + unit;
-    }
-    function getNumFromElement(id) {
-        const element = document.getElementById(id);
-        const str = element.innerText;
-        const num = parseFloat(str).toFixed(2);
-        return num;
-    }
     return function () {
         const cartList = document.getElementById("cartList");
-        const priceId = document.getElementById("priceId");
-        
-
+        const sell200Btn = document.getElementById("sell200-btn");
+        const purchaseBtn = document.getElementById("purchase-btn");
         //first step: dynamically adding li's
         const li = document.createElement("li");
         li.innerText = obj.name;
         cartList.appendChild(li);
 
         //second step: dynamically adding totalPrice 
-        const price = getNumFromElement(priceId);
+        const price = getNumFromElement("priceId");
         const netPrice = obj.price + price;
-        setNumToElement(priceId, netPrice, "TK");
+        const unit = netPrice + " " + "TK"
+        setNumToElement("priceId", unit);
+
+        //third step: converts the "Make Purchase" button blur to visible 
+        //when Total price > 0
+        if (netPrice > 0) {
+            purchaseBtn.style.opacity = 1;
+            purchaseBtn.style.pointerEvents = "auto";
+        }
+        //fourth step: converts the coupon button blur to visible 
+        //when totla price > 0
+        if (netPrice > 200) {
+            sell200Btn.style.opacity = 1;
+            sell200Btn.style.pointerEvents = "auto";
+        }
     }
 }
 
@@ -75,3 +90,23 @@ const item9 = clickHandlerProducer({
     name: "Flexible Sofa",
     price: 339.00
 })
+
+document.getElementById("sell200-btn").addEventListener("click", function (e) {
+    const couponId = document.getElementById("couponId");
+    if (couponId.value === "SELL200") {
+        const price = getNumFromElement("priceId");
+        const discount = (price * 20) / 100;
+        setNumToElement("discountId", discount);
+        const total = price - discount;
+        setNumToElement("totalPriceId", total);
+    }
+})
+
+function overlayMessageShow() {
+    let blackOverlay = document.getElementById("overlayId");
+    blackOverlay.style.display = "block";
+    
+
+
+   
+}
